204b529831d4df02f555fe2fb262fefc749fffa9
Alparslan İdris Arslan <46932194+4lparslan@users.noreply.github.com>
2021-02-17 17:26:11 +0300

    Create countdownTest.cs
    An example for using countdown timer.

---
+++ CountdownTimer/countdownTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countdownTest : MonoBehaviour
+{
+    countdown cs;
+    float startingTime;
+    float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cs = gameObject.AddComponent<countdown>();
+        cs.TotalTime = 3;
+        cs.Execute();
+        startingTime = Time.time; // Real world time based 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cs.Done)
+        {
+            Debug.Log(Time.time - startingTime); //elapsed time will be approximately 3 sec
+
+            startingTime = Time.time;
+            cs.Execute();
+        }
+    }
+}
